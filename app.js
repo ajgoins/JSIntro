7cc0298aaf1a2deda3506e60bb28a23deafddef0
ajgoins <42894572+ajgoins@users.noreply.github.com>
2018-09-04 19:37:00 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	<!--
+	  Name: Ashley Goins
+	  Page: app,js
+	  Date: 09/01/2018
+	-->
+		<title>
+			app.js My first attempt at JavaScript!
+		</title>
+	</head>
+	<body>
+    console.log("Ashley Goins");
+    console.log("35");
+    
+    console.log(6 * 3);
+    console.log(6 / 3);
+		console.log(6 + 3);
+    console.log(6 - 3);
+    
+    <p> 9, 4+5, 4 5</p>
+   </body>
+	
+</html>`
